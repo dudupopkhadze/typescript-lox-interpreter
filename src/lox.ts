8491f1d6cfdefeaf937cf206188ca95f79f7cabf
Dudu Popkhadze <dudupopkhadze@gmail.com>
2022-07-07 11:01:56 +0400

    prompt

--- src/lox.ts
+++ src/lox.ts
@@ -1,9 +1,23 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 export class Lox {
   start() {
     this.runPrompt();
   }
 
+  private handleInput(input: string) {
+    console.log(input);
+  }
+
   private runPrompt() {
-    console.log("propmt");
+    rl.question("> ", (input) => {
+      if (input === "exit") return rl.close();
+      this.handleInput(input);
+      this.runPrompt();
+    });
   }
 }
