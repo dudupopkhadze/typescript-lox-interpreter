715026e785dc2078fd60f3921a536c4197f1e7c7
Dudu Popkhadze <dudupopkhadze@gmail.com>
2022-07-09 01:49:56 +0400

    Scanner

--- src/Token.ts
+++ src/Token.ts
@@ -44,13 +44,15 @@ export enum TokenType {
   EOF,
 }
 
+export type Literal = unknown;
+
 export class Token {
   type: TokenType;
   lexeme: string;
-  literal: any;
+  literal: Literal;
   line: number;
 
-  constructor(type: TokenType, lexeme: string, literal: any, line: number) {
+  constructor(type: TokenType, lexeme: string, literal: Literal, line: number) {
     this.type = type;
     this.lexeme = lexeme;
     this.literal = literal;
