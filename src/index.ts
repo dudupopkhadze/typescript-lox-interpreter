6132fbf13c037232b7c85a9e3de779ed9ac48fde
nikasakana <nikasakana@gmail.com>
2022-09-06 16:29:42 +0400

    minor refactor/making code readble of main index.ts file

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,31 @@ import { InputSource, InputSourceFactory } from "./inputSource";
 import { Lox } from "./core/lox";
 import { LOX_VERSION } from "./util/constants";
 
+const executeFileCommands = (lox: Lox, fileName: string) => {
+  const inputSource: InputSource =
+    InputSourceFactory.createFileInputSource(fileName);
+  const input = inputSource.getInput();
+  input && lox.run(input);
+};
+
+const executeCliCommands = (lox: Lox) => {
+  const inputSource: InputSource = InputSourceFactory.createCliInputSource();
+  while (true) {
+    const input = inputSource.getInput();
+    if (!input) {
+      break;
+    }
+    lox.run(input);
+  }
+};
+
 const runLoxInterpreter = () => {
   const fileName = process.argv.length >= 3 ? process.argv[2] : null;
   const lox: Lox = new Lox();
-  const inputSource: InputSource = fileName
-    ? InputSourceFactory.createFileInputSource(fileName)
-    : InputSourceFactory.createCliInputSource();
   if (fileName) {
-    const input = inputSource.getInput();
-    input && lox.run(input);
+    executeFileCommands(lox, fileName);
   } else {
-    while (true) {
-      const input = inputSource.getInput();
-      if (!input) {
-        break;
-      }
-      lox.run(input);
-    }
+    executeCliCommands(lox);
   }
 };
 
